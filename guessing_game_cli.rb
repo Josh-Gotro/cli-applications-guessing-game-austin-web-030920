3e011a93f0dbbb0e8b4408e92c03a90f3dda38e7
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-03-03 18:56:55 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,24 @@
-# Code your solution here!
+def run_guessing_game
+  
+  # generate and store a random mnumber between 1 and 6
+  num = rand(6) + 1
+  
+  # Prompt user to guess their own number between 1 and 6
+  
+  # capture user input from the command link_to_remote
+  
+  
+  # Compare that input to the random number that has been generated
+  
+  # Print out one of the following statements:
+  
+  
+  # If the user's input matches the random number: You guessed the correct number!
+  
+  # If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
+  
+  # It the user's input is equal to "exit": Goodbye!
+  
+end
+
+#    ruby bin/guessing_game_cli
